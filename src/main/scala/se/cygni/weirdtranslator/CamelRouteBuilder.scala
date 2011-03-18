6f8d16a54724648a6982b99f727d86cb1dfee492
Martin Zachrison <zac@cyberzac.se>
2011-03-18 15:20:36 +0100

    Removed unnecessary import rename.

--- src/main/scala/se/cygni/weirdtranslator/CamelRouteBuilder.scala
+++ src/main/scala/se/cygni/weirdtranslator/CamelRouteBuilder.scala
@@ -1,21 +1,21 @@
 package se.cygni.weirdtranslator
 
-import org.apache.camel.scala.dsl.builder.{RouteBuilder => ScalaRouteBuilder}
 import org.apache.camel.Exchange
+import org.apache.camel.scala.dsl.builder.RouteBuilder
 
 trait CamelRouteBuilder extends ChainedTranslatorComponent {
 
-  def createRouteBuilder(minaPort: Int, xmppAddress: String): ScalaRouteBuilder = new ScalaRouteBuilder {
+  def createRouteBuilder(minaPort: Int, xmppAddress: String): RouteBuilder = new RouteBuilder {
 
     val xmpp = "xmpp://" + xmppAddress
     val mina = "mina:tcp://localhost:%s?textline=true&sync=true&encoding=UTF-8".format(minaPort)
 
-    mina process((exchange:Exchange) => {
+    xmpp bean (chainedTranslator) to xmpp
+
+    mina process ((exchange: Exchange) => {
       val translated = chainedTranslator.translate(exchange.in.toString)
       exchange.out = translated
     })
-
-    xmpp bean(chainedTranslator) to xmpp
 
   }
 
